20691d0e0a43ccf8c2d24617a51013681f637b69
omerokumus <omerokumus3@gmail.com>
2022-02-22 16:12:42 +0300

    intro to js

---
+++ intro.js
@@ -0,0 +1,56 @@
+// Variables
+
+// Declaration
+let x, y, z
+let i
+
+// Assignment
+x = 3
+y = 4
+
+// Single line
+let x2 = -2
+
+// Literal: kendi başlarına var olan ham datalardır. Değişkenlere atanır, fonksiyonlara arguman olarak gönderilir
+// 1 -> int literal
+// "js" -> string literal
+// false, true -> boolean literals
+// 1.2 -> double literal
+
+
+// Optional Semicolon
+// non-poblematic multi line
+x = 3
+y = 4
+
+// Problematic multi line
+let a
+=
+    3
+console.log(a)
+
+y = 3
+z = 4
+a = 2
+b = 9
+x = y + z;
+(a+b).toString();
+
+
+// var vs let
+if(true){
+    var g = 99;
+}
+console.log(g)
+
+if(true){
+    let p = 99;
+}
+console.log(p)
+
+
+// const vs let
+const pi = 3.14, e = 2.71
+let x2 = 4
+const y2 = -2
+x2 = y2 * 3
